a49f5a3ef3caa337bd7ac85feec2458b6efe8965
Martin Schweinberger <44954005+MartinSchweinberger@users.noreply.github.com>
2023-06-05 11:39:32 +1000

    Add files via upload

---
+++ scripts/rmwords.R
@@ -0,0 +1,9 @@
+# function to remove stopwords from a vector of strings
+# requires:
+# string: vector of strings
+# words: vector of words to be removed
+rm_words <- function(string, words) {
+  stopifnot(is.character(string), is.character(words))
+  spltted <- strsplit(string, " ", fixed = TRUE) # fixed = TRUE for speedup
+  vapply(spltted, function(x) paste(x[!tolower(x) %in% words], collapse = " "), character(1))
+}
